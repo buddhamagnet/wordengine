f7eaa9b0e0611497fc899933a148fd83ae1e84be
buddhamagnet <buddhamagnet@gmail.com>
2015-01-18 20:56:49 +0000

    chaos linkage

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,13 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  before_filter :randomise
+
+  def randomise
+    @drop = [
+      label: "stories", path: stories_path,
+      label: "poems", path: poems_path, param: Poem.sample,
+    ].sample
+  end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+	def randomise
+      [
+      	label: "stories", path: stories_path,
+      	label: "poems", path: poems_path, param: Poem.sample,
+      ].sample
+	end
 end
