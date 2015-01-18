cfbaa08547e82d49b00fab18daffda0bd94a4d84
buddhamagnet <buddhamagnet@gmail.com>
2015-01-18 17:54:28 +0000

    add page templates

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,9 @@
 class PagesController < ApplicationController
   def home
+  	render 'home'
+  end
+
+  def page
+    render params[:page]
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'pages/home'
+
+  get "pages/:page" => 'pages#page', as: 'page' 
 
   root 'pages#home'
 end
